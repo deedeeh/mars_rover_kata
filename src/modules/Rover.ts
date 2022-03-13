eb5c69af410ad0fa51651ae796eb441de55ccc67
deedeeh <dinahafez.8@gmail.com>
2022-03-13 12:33:51 +0000

    Implemented movement method functionality in Rover

--- src/modules/Rover.ts
+++ src/modules/Rover.ts
@@ -8,12 +8,70 @@ export default class Rover {
     this.y = y;
     this.direction = direction;
   }
-  //implement functionality for movement
-  movement(instructions: string) {
-    return instructions.split('');
+  
+  movement(instructions: string): void {
+    instructions.split('').forEach(letter => {
+      switch(letter) {
+        case 'L': 
+          switch(this.direction) {
+            case 'N':
+              this.direction = 'W';
+              break;
+            case 'S':
+              this.direction = 'E';
+              break;
+            case 'W':
+              this.direction = 'S';
+              break;
+            case 'E':
+              this.direction = 'N';
+              break;
+          }
+          break;
+        case 'R':
+          switch(this.direction) {
+            case 'N':
+              this.direction = 'E';
+              break;
+            case 'S':
+              this.direction = 'W';
+              break;
+            case 'W':
+              this.direction = 'N';
+              break;
+            case 'E':
+              this.direction = 'S';
+              break;
+          }
+          break;
+        case 'M':
+          switch(this.direction) {
+            case 'N':
+              this.y += 1;
+              break;
+            case 'S':
+              this.y -= 1;
+              break;
+            case 'W':
+              this.x -= 1;
+              break;
+            case 'E':
+              this.x += 1;
+              break;
+          }
+          break;
+          default:
+            return `Sorry, there is no ${letter} as a rover moving instruction`
+      }
+    });
   }
 }
 
 const rover1 = new Rover(1, 2, 'N');
+const rover2 = new Rover(3, 3, 'E');
 
-console.log(rover1.movement('LMLM'));
+rover1.movement('LMLMLMLMM');
+rover2.movement('MMRMMRMRRM');
+
+console.log(rover1)
+console.log(rover2)
