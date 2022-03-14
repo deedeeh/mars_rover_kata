4240be850e61d1f13e7cf5c0fe9e2ee2e9d1b71e
deedeeh <dinahafez.8@gmail.com>
2022-03-14 16:34:54 +0000

    Validated that given plateau coordinates are bigger or equal to rover

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,6 +4,18 @@ import Rover from '../src/modules/Rover';
 
 describe('moveRoverOnPlateau', () => {
 
+  test.each([ {plateauX: 5, plateauY: 5, roverX: 10, roverY: 5, direction: 'N', instructions: 'LM'},
+  {plateauX: 5, plateauY: 5, roverX: 4, roverY: 12, direction: 'N', instructions: 'LMLMLMLMM'},
+  {plateauX: 5, plateauY: 5, roverX: 8, roverY: 8, direction: 'E', instructions: 'MMRMMRMRRM'},
+])('throws an error if rover coordinates ($roverX, $roverY) are bigger than plateau coordinates ($plateauX, $plateauY)', ({plateauX, plateauY, roverX, roverY, direction, instructions}) => {
+  const plateau = new Plateau(plateauX, plateauY);
+  const rover = new Rover(roverX, roverY, direction);
+  const roverInstructions = instructions;
+  expect(() => {
+    moveRoverOnPlateau(plateau, rover, roverInstructions);
+  }).toThrow('Plateau coordinates must be bigger than or equal to rover coordinates.')
+});
+
   test.each([
     {plateauX: 5, plateauY: 5, roverX: 1, roverY: 1, direction: 'N', instructions: 'LM', expected: '0, 1, W'},
     {plateauX: 5, plateauY: 5, roverX: 1, roverY: 2, direction: 'N', instructions: 'LMLMLMLMM', expected: '1, 3, N'},
