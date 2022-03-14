227ae5644731e5fac385378a132e4c0a51062658
deedeeh <dinahafez.8@gmail.com>
2022-03-14 22:54:34 +0000

    Implemented the logic of rover to move in plateau range

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,104 @@ import Plateau from "./modules/Plateau"
 import Rover from "./modules/Rover"
 
 export const moveRoverOnPlateau = (plateau: Plateau, rover: Rover, instructions: string): string => {
-  const marsPlateau = new Plateau(plateau.x, plateau.y);
-  const theRover = new Rover(rover.x, rover.y, rover.direction);
+  let isValid = false;
 
-  if(marsPlateau.x >= theRover.x && marsPlateau.y >= theRover.y) {
-    theRover.movement(instructions);
+  if(plateau.x >= rover.x && plateau.y > rover.y) {
+    isValid = isValidMovement(plateau, rover, instructions);
   } else {
-    throw new Error('Plateau coordinates must be bigger than or equal to rover coordinates.');
-  }
-
-  if(theRover.x > marsPlateau.x || theRover.y > marsPlateau.y) {
-    throw new Error('Rover coordinates will be bigger than plateau so rover stopped moving')
+    throw new Error('Plateau given coordinates must be bigger than or equal to rover given coordinates.');
   }
   
-  return `${theRover.x}, ${theRover.y}, ${theRover.direction}`
+  if(isValid === true) {
+    return `${rover.x}, ${rover.y}, ${rover.direction}`
+  } else {
+    return `Rover is going to be out of plateau range. It stopped moving at coordinates (${rover.x}, ${rover.y}, ${rover.direction})`
+  }
+}
+
+/** 
+   * Implements the functionality and logic behind moving the rover on Mars without falling off the Plateau range
+   * @param plateau - type Plateau 
+   * @param rover - type Rover 
+   * @param instructions - given a string of letters to move the Rover; R, L, M  
+  */
+
+const isValidMovement = (plateau: Plateau, rover: Rover, instructions: string): boolean => {
+  let isValid = true; 
+
+  instructions.toUpperCase().split('').forEach(letter => {
+    switch(letter) {
+      case 'L': 
+        switch(rover.direction) {
+          case 'N':
+            rover.direction = 'W';
+            break;
+          case 'S':
+            rover.direction = 'E';
+            break;
+          case 'W':
+            rover.direction = 'S';
+            break;
+          case 'E':
+            rover.direction = 'N';
+            break;
+        }
+        break;
+      case 'R':
+        switch(rover.direction) {
+          case 'N':
+            rover.direction = 'E';
+            break;
+          case 'S':
+            rover.direction = 'W';
+            break;
+          case 'W':
+            rover.direction = 'N';
+            break;
+          case 'E':
+            rover.direction = 'S';
+            break;
+        }
+        break;
+      case 'M': 
+        switch(rover.direction) {
+          case 'N':
+            if(plateau.y > rover.y) {
+              rover.y += 1; 
+              isValid;
+            } else {
+              isValid = false;
+            }
+          break;
+          case 'S':
+            if(rover.y > 0) {
+              rover.y -= 1; 
+              isValid;
+            } else {
+              isValid = false;
+            }
+          break;
+          case 'W':
+            if(rover.x > 0) {
+              rover.x -= 1;
+              isValid;
+            } else {
+              isValid = false
+            }
+          break
+          case 'E':
+            if(plateau.x > rover.x) {
+              rover.x += 1;
+              isValid;
+            } else {
+              isValid = false;
+            }
+          break;
+        }
+      break;
+      default: 
+        throw new Error('Instructions may include letter L, R and M');
+      }
+  });
+  return isValid;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,18 +4,6 @@ import Rover from '../src/modules/Rover';
 
 describe('moveRoverOnPlateau', () => {
 
-  test.each([ {plateauX: 5, plateauY: 5, roverX: 5, roverY: 5, direction: 'E', instructions: 'MM'},
-  {plateauX: 5, plateauY: 5, roverX: 3, roverY: 4, direction: 'S', instructions: 'LMMMMRMLMM'},
-  {plateauX: 4, plateauY: 4, roverX: 3, roverY: 3, direction: 'W', instructions: 'RMM'},
-])('throws an error if rover coordinates will be bigger than plateau coordinates after moving', ({plateauX, plateauY, roverX, roverY, direction, instructions}) => {
-  const plateau = new Plateau(plateauX, plateauY);
-  const rover = new Rover(roverX, roverY, direction);
-  const roverInstructions = instructions;
-  expect(() => {
-    moveRoverOnPlateau(plateau, rover, roverInstructions);
-  }).toThrow('Rover coordinates will be bigger than plateau so rover stopped moving');
-});
-
   test.each([ {plateauX: 5, plateauY: 5, roverX: 10, roverY: 5, direction: 'N', instructions: 'LM'},
   {plateauX: 5, plateauY: 5, roverX: 4, roverY: 12, direction: 'N', instructions: 'LMLMLMLMM'},
   {plateauX: 5, plateauY: 5, roverX: 8, roverY: 8, direction: 'E', instructions: 'MMRMMRMRRM'},
@@ -25,7 +13,7 @@ describe('moveRoverOnPlateau', () => {
   const roverInstructions = instructions;
   expect(() => {
     moveRoverOnPlateau(plateau, rover, roverInstructions);
-  }).toThrow('Plateau coordinates must be bigger than or equal to rover coordinates.')
+  }).toThrow('Plateau given coordinates must be bigger than or equal to rover given coordinates.')
 });
 
   test.each([
